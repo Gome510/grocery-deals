c276f7b948f1fb740c113d44cc5258c2612bac25
Gome510 <bluehawkfire55@gmail.com>
2023-04-09 11:08:10 -0700

    Create StoreItem.jsx

---
+++ grocerydeal/src/js/StoreItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function StoreItem(){
+    return(
+        <a href="#" class="list-group-item list-group-item-action active" data-bs-toggle="list"  role="tab" aria-current="true">
+            <div class="d-flex w-100 justify-content-between">
+                <h5 class="mb-1">Store Name</h5>
+                <small>Distance</small>
+                <small>Hours</small>
+            </div>
+            <p class="mb-1">Total Price</p>
+            <small>And some small print.</small>
+        </a>
+    )
+}
